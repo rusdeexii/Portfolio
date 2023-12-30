34a6bd650ec49fdbd274c13b4a757281bf559d79
กษมา สามารถ <macbook_air@ksmas-MacBook-Air.local>
2023-12-30 16:34:42 +0700

    Clean repository

--- src/utils/anchor-items.jsx
+++ src/utils/anchor-items.jsx
@@ -1,27 +1,29 @@
+
 import {
     HomeOutlined,
     UserOutlined,
     LaptopOutlined,
     MessageOutlined,
 } from "@ant-design/icons";
+import React from "react";
 
 export const ANCHOR_ITEMS = [
     {
         title: (
-            <>
+            <React.Fragment>
                 <HomeOutlined className="mr-2" />
                 Home
-            </>
+            </React.Fragment>
         ),
         key: "section-home",
         href: "#section-home",
     },
     {
         title: (
-            <>
+            <React.Fragment>
                 <UserOutlined className="mr-2" />
                 About Me
-            </>
+            </React.Fragment>
         ),
         key: "section-about-me",
         href: "#section-about-me",
@@ -29,20 +31,20 @@ export const ANCHOR_ITEMS = [
   ,
     {
         title: (
-            <>
+            <React.Fragment>
                 <LaptopOutlined className="mr-2" />
                 Skills
-            </>
+            </React.Fragment>
         ),
         key: "section-skills",
         href: "#section-skills",
     },
     {
         title: (
-            <>
+            <React.Fragment>
                 <MessageOutlined className="mr-2" />
                 Contact Me
-            </>
+            </React.Fragment>
         ),
         key: "section-contact-me",
         href: "#section-contact-me",
