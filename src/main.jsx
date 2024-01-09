4503fa7e75c0ba7fa5cd4cbf7359fe5e512609d2
rusdeexii <125761360+rusdeexii@users.noreply.github.com>
2024-01-09 23:01:26 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
