f05f4b1970e7a0070666aa445269ca2cc3444da1
rusdeexii <125761360+rusdeexii@users.noreply.github.com>
2024-01-09 23:52:09 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
-process.env.NODE_ENV = 'production';
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 
